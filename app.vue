a6715181b2c13d30e62578f63ad2e2f8aa3bf396
TRUNGLEPC <lmt.cn.tv@gmail.com>
2023-04-12 23:31:11 +0700

    Car card list option

--- app.vue
+++ app.vue
@@ -1,36 +1,8 @@
 <template>
   <div>
-    <header
-      class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-sm"
-    >
+    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-sm">
       <a href="" class="text-3xl font-mono">Cartrader</a>
     </header>
-    <!-- Car page -->
-    <div
-      class="mx-auto mt-4 max-w-7xl space-x-4 px-4 xs:px-8 sm:px-10 lg:px-16 w-3/5"
-    >
-      <div class="mt-32 flex">
-        <!-- car side bar -->
-        <div class="shadow border w-64 mr-10 z-30 h-[190px]">
-          <div
-            class="p-5 flex justify-between relative cursor-pointer border-b"
-          >
-            <h3>Location</h3>
-            <h3 class="text-blue-400 capitalize">Toronto</h3>
-            <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
-              <input type="text" class="border p-1 rounded">
-              <button class="bg-blue-400 w-full mt-2 rounded text-white p1">
-                Apply
-              </button>
-            </div>
-          </div>
-        </div>
-        <!-- car side bar -->
-        <!-- Car card -->
-
-        <!-- Car card -->
-      </div>
-    </div>
-    <!-- Car page -->
+    
   </div>
 </template>
